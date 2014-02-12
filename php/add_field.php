6c98099a53896a4fe393ac5979a637d3db3e2b35
jquindlen <jquindlen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-12 10:00:23 +0000

    = 4.3.1 = * Fixed: Patched a bug that was preventing guests from posting
    git-svn-id: https://plugins.svn.wordpress.org/wpsc-support-tickets/tags/4.3.1@856262 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/add_field.php
@@ -0,0 +1,44 @@
+<?php
+
+error_reporting(0);
+
+if (!function_exists('add_action'))
+{
+    require_once("../../../../wp-config.php");
+}
+
+global $current_user, $wpdb;
+
+wp_get_current_user();
+if ( 0 == $current_user->ID ) {
+    // Not logged in.
+} else {
+
+    if (function_exists('current_user_can') && !current_user_can('manage_wpsct_support_tickets') ) {
+        die(__('Unable to Authenticate', 'wpsc-support-tickets'));
+    }
+
+    $table_name = $wpdb->prefix . "wpstorecart_meta";
+    $createnewfieldname = $wpdb->escape($_POST['createnewfieldname']);
+    $createnewfieldtype = $wpdb->escape($_POST['createnewfieldtype']);
+    $createnewfieldrequired = $wpdb->escape($_POST['createnewfieldrequired']);
+
+    $insert = "
+    INSERT INTO `{$table_name}` (
+    `primkey`, `value`, `type`, `foreignkey`)
+    VALUES (
+            NULL,
+            '{$createnewfieldname}||{$createnewfieldrequired}||{$createnewfieldtype}',
+            'wpst-requiredinfo',
+            '0'
+    );
+    ";
+
+    $results = $wpdb->query($insert);
+    $lastID = $wpdb->insert_id;
+    echo $lastID;
+
+
+}
+
+?>
